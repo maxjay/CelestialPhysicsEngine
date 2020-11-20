e13d613f8bdfef29e483024b56b63bf01bd94201
maxjay <disgracedandbetrayed@gmail.com>
2020-11-20 19:32:37 +0000

    start of a physics engine, test graphics engine

---
+++ graphics.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+
+pygame.init()
+screen = pygame.display.set_mode([500, 500])
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(screen, (0, 0, 255), (random.randint(0, 255), random.randint(0, 255)), 75)
+    pygame.display.flip()
+pygame.quit()
--- physics.py
+++ physics.py
@@ -2,3 +2,34 @@ from scipy import constants
 
 G = constants.gravitational_constant
 c = constants.speed_of_light
+
+class Physics:
+    def __init__(self):
+        self.objects = []
+
+    def addPlanet(self, planet):
+        self.objects.append(planet)
+
+    @staticmethod
+    def componentForce(resultantForce):
+        return 1,1
+
+    @staticmethod
+    def gravitationalForce(planetA, planetB):
+        return 1
+
+    def updatePos(self, resultForce, planet):
+        planet.vx, planet.vy = Physics.componentForce(resultForce)
+
+    def applyForces(self, forces):
+        for force, planet in zip(forces, self.objects):
+            self.updatePos(force, planet)
+
+    def compute(self):
+        forces = [0 for i in xrange(len(self.objects))]
+        for i in xrange(len(self.objects)):
+            a = self.objects[i]
+            for j in xrange(i+1, len(self.objects)):
+                forces[i] += self.gravitationalForce(a, self.objects[j])
+        self.applyForces(forces)
+
